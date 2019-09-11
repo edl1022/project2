dd8e4a98f4fc564321845ae38817e951ded99e45
Nico Barbarese <nicobarbarese@gmail.com>
2019-09-10 20:53:30 -0400

    the sign is wortking

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 // import { get } from "https"
 
-$(document).ready(function() {
+$(document).ready(function () {
     //   // The API object contains methods for each kind of request we'll make
     //   var sign = $("signs").val().trim()
     //   var API = {
@@ -37,42 +37,46 @@ $(document).ready(function() {
     //       });
     //     }
     //   };
-    
+
     function horoscope() {
-        var sign ;
+        var sign;
         $.ajax({
             type: "GET",
             url: "/api/getsign"
-        }).then(function(data){
+        }).then(function (data) {
             // console log getting sign
-            console.log("Astrological Sign: " + data)
-        })
-        $.ajax({
-            // headers: {
-            //     "Content-Type": "application/json"
-            // },
-            type: "GET",
-            url: "http://sandipbgt.com/theastrologer/api/horoscope/capricorn/today/"
+            console.log("Astrological Sign: " + JSON.stringify(data.sign))
+            sign = data.sign.split(" ")
+            sign = sign[0].toLowerCase()
+            console.log(sign)
+        }).then(function(){
+            $.ajax({
+                // headers: {
+                //     "Content-Type": "application/json"
+                // },
+                type: "GET",
+                url: "http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/today/"
                 // data: console.log(JSON.stringify(data))
-        }).then(function(data) {
-            var parseData = JSON.parse(data)
-            var horoscopeDisplay = $("#today")
-            var newDiv = $("<div>")
-            var newText = $("<p>")
+            }).then(function (data) {
+                var parseData = JSON.parse(data)
+                var horoscopeDisplay = $("#today")
+                var newDiv = $("<div>")
+                var newText = $("<p>")
 
-            var newDate = $("<p>")
-            
-            newText.text(parseData.horoscope)
-            newDate.text(parseData.date)
-            newDiv.addClass("today-horoscope")
-            newDiv.append(newText)
-            newDiv.append(newDate)
-            horoscopeDisplay.append(newDiv)
+                var newDate = $("<p>")
 
+                newText.text(parseData.horoscope)
+                newDate.text(parseData.date)
+                newDiv.addClass("today-horoscope")
+                newDiv.append(newText)
+                newDiv.append(newDate)
+                horoscopeDisplay.append(newDiv)
+
+            })
         })
     }
 
-    $("#heart").on("click", function(){
+    $("#heart").on("click", function () {
         console.log("Success!")
     })
 

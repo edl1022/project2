f0ab4095cca25a1d902d3a9b2d7ae2260146e203
edaniels2275 <edaniels2275@gmail.com>
2019-09-12 20:54:41 -0400

    local-storage-work

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,18 @@
 // import { get } from "https"
 
-$(document).ready(function () {
-
+$(document).ready(function() {
+    console.log(`gettting favorites for user: ${localStorage.getItem('userID')}`)
+    var id = JSON.parse(localStorage.getItem('userID'))
+    console.log(id.toString())
+    console.log('33')
     $.ajax({
         url: "/api/favorites",
         type: "POST",
-        data: localStorage.getItem('userID')
-    }).then(function (response) {
+        data: {
+            userID: id.toString()
+        }
+    }).then(function(response) {
+        console.log(response)
         var horoscopes = response.data.horoscopes
         for (var i = 0; i < horoscopes.length; i++) {
             makeHoroscopes(horoscopes[i])
@@ -19,18 +25,18 @@ $(document).ready(function () {
         $.ajax({
             type: "GET",
             url: "/api/getsign"
-        }).then(function (data) {
+        }).then(function(data) {
             sign = data.sign.split(" ")
             sign = sign[0].toLowerCase()
-        }).then(function () {
+        }).then(function() {
             $.ajax({
                 // headers: {
                 //     "Content-Type": "application/json"
                 // },
                 type: "GET",
                 url: "http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/today/"
-                // data: console.log(JSON.stringify(data))
-            }).then(function (data) {
+                    // data: console.log(JSON.stringify(data))
+            }).then(function(data) {
                 var parseData = JSON.parse(data)
                 var horoscopeDisplay = $("#today")
                 var newDiv = $("<div>")
@@ -41,14 +47,14 @@ $(document).ready(function () {
                 newText.addClass("today-horoscope")
                 newDate.text(parseData.date)
                 newDiv.append(newText)
-                // newDiv.append(newDate)
+                    // newDiv.append(newDate)
                 horoscopeDisplay.append(newDiv)
 
             })
         })
     }
 
-    $("#heart").on("click", function () {
+    $("#heart").on("click", function() {
         console.log("Saving: ", $(".today-horoscope").text())
         saveHoroscope($(".today-horoscope").text())
 
